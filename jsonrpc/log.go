d6c6974017762f75944f0a64c419f7d5d54bbd74
Ori Newman <orinewman1@gmail.com>
2020-02-16 16:59:37 +0200

    [NOD-754] Fix staticcheck errors

--- jsonrpc/log.go
+++ jsonrpc/log.go
@@ -2,13 +2,11 @@ package jsonrpc
 
 import (
 	"github.com/kaspanet/kaspad/rpcclient"
-	"github.com/kaspanet/kaspad/util/panics"
 	"github.com/kaspanet/kasparov/logger"
 )
 
 var (
-	log   = logger.BackendLog.Logger("RPCC")
-	spawn = panics.GoroutineWrapperFunc(log)
+	log = logger.BackendLog.Logger("RPCC")
 )
 
 func init() {
