a9d470d66094c2b14205f212832f30118b261a33
Dan Aharoni <dan@daglabs.com>
2020-03-08 14:09:39 +0200

    [NOD-773] Limit `bits` to maxuint32

--- database/migrations/000001_create_blocks_table.up.sql
+++ database/migrations/000001_create_blocks_table.up.sql
@@ -8,7 +8,7 @@ CREATE TABLE blocks
     accepted_id_merkle_root CHAR(64) NOT NULL,
     utxo_commitment         CHAR(64) NOT NULL,
     timestamp               TIMESTAMP(0) NOT NULL,
-    bits                    BIGINT CHECK (bits >= 0) NOT NULL,
+    bits                    BIGINT CHECK (bits >= 0 and bits <= 4294967295) NOT NULL, -- bits should be in range of uint32
     nonce                   BYTEA,
     blue_score              BIGINT CHECK (blue_score >= 0) NOT NULL,
     is_chain_block          BOOLEAN NOT NULL,
