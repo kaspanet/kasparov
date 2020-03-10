85fc861570d757b98833a5e79fbe9adc3e535659
Dan Aharoni <dan@daglabs.com>
2020-03-10 15:33:03 +0200

    [NOD-773] Add foreign key for subnetwork_id

--- database/migrations/000005_create_transactions_table.up.sql
+++ database/migrations/000005_create_transactions_table.up.sql
@@ -5,7 +5,7 @@ CREATE TABLE transactions
     transaction_hash   CHAR(64) NOT NULL,
     transaction_id     CHAR(64) NOT NULL,
     lock_time          BYTEA NOT NULL,
-    subnetwork_id      BIGINT CHECK (subnetwork_id >= 0) NOT NULL,
+    subnetwork_id      BIGINT NOT NULL,
     gas                BIGINT CHECK (gas >= 0) NOT NULL,
     payload_hash       CHAR(64) NOT NULL,
     payload            BYTEA NOT NULL,
@@ -15,7 +15,11 @@ CREATE TABLE transactions
     CONSTRAINT idx_transactions_transaction_hash UNIQUE (transaction_hash),
     CONSTRAINT fk_transactions_accepting_block_id
         FOREIGN KEY (accepting_block_id)
-            REFERENCES blocks (id)
+            REFERENCES blocks (id),
+    CONSTRAINT fk_transactions_subnetwork_id
+        FOREIGN KEY (subnetwork_id)
+            REFERENCES subnetworks (id)
+
 );
 
 CREATE INDEX idx_transactions_transaction_id ON transactions (transaction_id);
