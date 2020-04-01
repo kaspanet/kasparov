df4cac882ae46826f052ebecdd8c6f4349266d0e
Mike Zak <feanorr@gmail.com>
2020-04-01 08:48:44 +0300

    [NOD-886] Update cli-wallet to use go-secp256k1

--- examples/wallet/create.go
+++ examples/wallet/create.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/kaspanet/go-secp256k1"
 	"github.com/kaspanet/kaspad/dagconfig"
-	"github.com/kaspanet/kaspad/ecc"
 	"github.com/kaspanet/kaspad/util"
 )
 
 func create() {
-	privateKey, err := ecc.NewPrivateKey(ecc.S256())
+	privateKey, err := secp256k1.GeneratePrivateKey()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to generate private key: %s", err)
 		os.Exit(1)
@@ -21,7 +21,18 @@ func create() {
 
 	fmt.Println("These are your public addresses for each network, where money is to be sent.")
 	for _, netParams := range []*dagconfig.Params{&dagconfig.MainnetParams, &dagconfig.TestnetParams, &dagconfig.DevnetParams} {
-		addr, err := util.NewAddressPubKeyHashFromPublicKey(privateKey.PubKey().SerializeCompressed(), netParams.Prefix)
+		publicKey, err := privateKey.SchnorrPublicKey()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to generate public key: %s", err)
+			os.Exit(1)
+		}
+		publicKeySerialized, err := publicKey.SerializeCompressed()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to generate public key: %s", err)
+			os.Exit(1)
+		}
+
+		addr, err := util.NewAddressPubKeyHashFromPublicKey(publicKeySerialized, netParams.Prefix)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to generate p2pkh address: %s", err)
 			os.Exit(1)
