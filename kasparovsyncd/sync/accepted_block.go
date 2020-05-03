88561646f2ebeb7eb8502ed648597e0c8a8ad206
Ori Newman <orinewman1@gmail.com>
2020-05-03 15:33:32 +0300

    [NOD-944] Return slice instead of nil

--- kasparovsyncd/sync/accepted_block.go
+++ kasparovsyncd/sync/accepted_block.go
@@ -30,7 +30,7 @@ func insertAcceptedBlocks(dbTx *database.TxContext, blocks []*rawAndVerboseBlock
 func dbAcceptedBlocksFromVerboseBlock(blockHashesToIDs map[string]uint64, verboseBlock *rpcmodel.GetBlockVerboseResult) ([]*dbmodels.AcceptedBlock, error) {
 	// Exit early if this is the genesis block
 	if len(verboseBlock.AcceptedBlockHashes) == 0 {
-		return nil, nil
+		return []*dbmodels.AcceptedBlock{}, nil
 	}
 
 	blockID, ok := blockHashesToIDs[verboseBlock.Hash]
